5763f9f42d4bcc3117eb910c24d7ab951362d32d
Justin <goshornjustin@gmail.com>
2024-10-25 08:23:05 -0400

    added a web scrape testing file

---
+++ test.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import requests
+
+
+url = 'https://www.scrapethissite.com/pages/forms/'
+
+BASE_HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+                "accept": 
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "accept-langauge": "en-US",
+                "accept-encoding": "gzip,deflate,br,zstd"
+                }
+
+page = requests.get(url)
+html = page.content
+soup = BeautifulSoup(html, "html.parser")
+
+table = soup.find("table",class_="table")
+
+for i in table:
+    print()
